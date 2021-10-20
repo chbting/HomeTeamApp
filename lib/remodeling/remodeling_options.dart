4d4d2df083e782c217e2af10fbb167c09437d2ed
Benjamin Ting <chbting@gmail.com>
2021-10-20 20:23:51 +0800

    Added estimate demo calculation and wall coverings section

--- lib/remodeling/remodeling_options.dart
+++ lib/remodeling/remodeling_options.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:intl/intl.dart';
 import 'package:tner_client/remodeling/remodeling_selections.dart';
 
 class RemodelingOptionsScreen extends StatefulWidget {
@@ -18,7 +19,14 @@ class RemodelingOptionsScreen extends StatefulWidget {
 class RemodelingOptionsScreenState extends State<RemodelingOptionsScreen>
     with AutomaticKeepAliveClientMixin {
   List<Widget> _optionsList = [];
+
+  // Painting Card
+  int? _paintArea, _paintRooms;
   bool? _scrapeOldPaint;
+
+  // Painting Card
+  int? _wallCoveringsArea, _wallCoveringsRooms;
+  bool? _removeOldWallCoverings;
 
   @override
   bool get wantKeepAlive => true;
@@ -43,42 +51,47 @@ class RemodelingOptionsScreenState extends State<RemodelingOptionsScreen>
             children: [
               Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    AppLocalizations.of(context)!.painting,
-                    style: Theme.of(context).textTheme.headline6!.copyWith(
-                        color: Theme.of(context)
-                            .colorScheme
-                            .secondary), // TODO use custom function
-                  )),
+                  child: Text(AppLocalizations.of(context)!.painting,
+                      style: _getItemTitleTextStyle())),
               Row(
                 children: [
                   Expanded(
                       child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: TextField(
-                        keyboardType: TextInputType.number,
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly
-                        ],
-                        textInputAction: TextInputAction.next,
-                        decoration: InputDecoration(
-                          border: const OutlineInputBorder(),
-                          labelText: AppLocalizations.of(context)!.area_sq_ft,
-                        )),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      textInputAction: TextInputAction.next,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: AppLocalizations.of(context)!.area_sq_ft,
+                      ),
+                      onChanged: (value) {
+                        value.isEmpty
+                            ? _paintArea = null
+                            : _paintArea = int.parse(value);
+                        setState(() {});
+                      },
+                    ),
                   )),
                   Expanded(
                       child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: TextField(
-                        keyboardType: TextInputType.number,
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly
-                        ],
-                        decoration: InputDecoration(
-                          border: const OutlineInputBorder(),
-                          labelText:
-                              AppLocalizations.of(context)!.number_of_rooms,
-                        )),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText:
+                            AppLocalizations.of(context)!.number_of_rooms,
+                      ),
+                      onChanged: (value) {
+                        value.isEmpty
+                            ? _paintRooms = null
+                            : _paintRooms = int.parse(value);
+                        setState(() {});
+                      },
+                    ),
                   )),
                 ],
               ),
@@ -119,7 +132,7 @@ class RemodelingOptionsScreenState extends State<RemodelingOptionsScreen>
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Padding(//todo color
+                  Padding(
                       padding: const EdgeInsets.symmetric(
                           vertical: 8.0, horizontal: 16.0),
                       child: Text(AppLocalizations.of(context)!.estimate,
@@ -127,7 +140,14 @@ class RemodelingOptionsScreenState extends State<RemodelingOptionsScreen>
                   Padding(
                       padding: const EdgeInsets.symmetric(
                           vertical: 8.0, horizontal: 16.0),
-                      child: Text('\$1000',
+                      child: Text(
+                          _getPaintingEstimate() == null
+                              ? '\$-'
+                              : NumberFormat.currency(
+                                      locale: 'zh_HK',
+                                      symbol: '\$',
+                                      decimalDigits: 0)
+                                  .format(_getPaintingEstimate()),
                           textAlign: TextAlign.right,
                           style: Theme.of(context).textTheme.subtitle1)),
                 ],
@@ -138,7 +158,119 @@ class RemodelingOptionsScreenState extends State<RemodelingOptionsScreen>
       ));
     }
     if (widget.selectionMap[RemodelingOptions.wallCoveringsKey]!) {
-      //_optionsList.add(value);
+      _optionsList.add(Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(AppLocalizations.of(context)!.wallcoverings,
+                      style: _getItemTitleTextStyle())),
+              Row(
+                children: [
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      textInputAction: TextInputAction.next,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: AppLocalizations.of(context)!.area_sq_ft,
+                      ),
+                      onChanged: (value) {
+                        value.isEmpty
+                            ? _wallCoveringsArea = null
+                            : _wallCoveringsArea = int.parse(value);
+                        setState(() {});
+                      },
+                    ),
+                  )),
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText:
+                            AppLocalizations.of(context)!.number_of_rooms,
+                      ),
+                      onChanged: (value) {
+                        value.isEmpty
+                            ? _wallCoveringsRooms = null
+                            : _wallCoveringsRooms = int.parse(value);
+                        setState(() {});
+                      },
+                    ),
+                  )),
+                ],
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 16.0),
+                  child: Text(AppLocalizations.of(context)!.scrape_old_paint,
+                      style: Theme.of(context).textTheme.subtitle1)),
+              Row(
+                children: [
+                  Expanded(
+                    child: RadioListTile(
+                      title: Text(
+                          AppLocalizations.of(context)!.scrape_old_paint_yes),
+                      value: true,
+                      groupValue: _removeOldWallCoverings,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _removeOldWallCoverings = true;
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                      child: RadioListTile(
+                    title:
+                        Text(AppLocalizations.of(context)!.scrape_old_paint_no),
+                    value: false,
+                    groupValue: _removeOldWallCoverings,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        _removeOldWallCoverings = false;
+                      });
+                    },
+                  )),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 16.0),
+                      child: Text(AppLocalizations.of(context)!.estimate,
+                          style: Theme.of(context).textTheme.subtitle1)),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 16.0),
+                      child: Text(
+                          _getWallCoveringsEstimate() == null
+                              ? '\$-'
+                              : NumberFormat.currency(
+                                      locale: 'zh_HK',
+                                      symbol: '\$',
+                                      decimalDigits: 0)
+                                  .format(_getWallCoveringsEstimate()),
+                          textAlign: TextAlign.right,
+                          style: Theme.of(context).textTheme.subtitle1)),
+                ],
+              )
+            ],
+          ),
+        ),
+      ));
     }
     if (widget.selectionMap[RemodelingOptions.acInstallationKey]!) {
       //_optionsList.add(value);
@@ -174,6 +306,33 @@ class RemodelingOptionsScreenState extends State<RemodelingOptionsScreen>
           children: _optionsList,
         ));
   }
-}
 
-class PaintingOptionsWidget {}
+  int? _getPaintingEstimate() {
+    if (_paintArea == null || _paintRooms == null || _scrapeOldPaint == null) {
+      return null;
+    } else {
+      // TODO
+      return _scrapeOldPaint!
+          ? (_paintArea! + _paintRooms!) * 2
+          : _paintArea! + _paintRooms!;
+    }
+  }
+
+  int? _getWallCoveringsEstimate() {
+    if (_wallCoveringsArea == null ||
+        _wallCoveringsRooms == null ||
+        _removeOldWallCoverings == null) {
+      return null;
+    } else {
+      // TODO
+      return _removeOldWallCoverings!
+          ? (_wallCoveringsArea! + _wallCoveringsRooms!) * 2
+          : _wallCoveringsArea! + _wallCoveringsRooms!;
+    }
+  }
+
+  TextStyle _getItemTitleTextStyle() => Theme.of(context)
+      .textTheme
+      .headline6!
+      .copyWith(color: Theme.of(context).colorScheme.secondary);
+}
--- lib/theme.dart
+++ lib/theme.dart
@@ -6,21 +6,7 @@ class AppTheme {
   static const Color lightThemeAccent = Colors.white;
   static final Color? darkThemeBackground = Colors.grey[900];
   static final Color tnerBlue = Colors.lightBlueAccent[700]!;
-  static const MaterialColor lightBlue = MaterialColor(
-    0xFF29B6F6,
-    <int, Color>{
-      50: Color(0xFFE1F5FE),
-      100: Color(0xFFB3E5FC),
-      200: Color(0xFF81D4FA),
-      300: Color(0xFF4FC3F7),
-      400: Color(0xFF29B6F6),
-      500: Color(0xFF29B6F6),
-      600: Color(0xFF039BE5),
-      700: Color(0xFF0288D1),
-      800: Color(0xFF0277BD),
-      900: Color(0xFF01579B),
-    },
-  );
+
   static ThemeData getDarkTheme() {
     return ThemeData(
       brightness: Brightness.dark,
